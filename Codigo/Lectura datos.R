f3ce0d8fbaa3b11998e0e862095a151a0b3327aa
Axel <axel.gonzlez@uc.cl>
2022-10-22 17:51:30 -0300

    Creacion funcion partidos, avance tabla y avance ggplot para UC

--- Codigo/Lectura datos.R
+++ Codigo/Lectura datos.R
@@ -43,11 +43,11 @@ puntos <- function(dato){
 
 #PRUEBA LA FUNCION CREADA CON LOS DATOS ANTERIORMENTE PROBADOS
 local <- Colo_local %>% 
-  filter(año == "2008") %>% 
+  filter(año == "2009") %>% 
   puntos()
 
 visita <- Colo_v %>% 
-  filter(año == "2008") %>% 
+  filter(año == "2009") %>% 
   puntos()
 
 rbind(local,visita) #interpretar bien donde esta cada dato
@@ -56,11 +56,11 @@ rbind(local,visita) #interpretar bien donde esta cada dato
 
 total <- c(local[1] + visita[3],local[2] + visita[2],local[3] + visita[1],local[4] + visita[6],local[5] + visita[5],local[6] + visita[4])
 
-#local[1] + visita[3] = PARTIDOS GANADOS 
+#local[1] + visita[3] = PUNTOS GANADOS 
 #local[2] + visita[2] = PARTIDOS EMPATADOS
-#local[3] + visita[1] = PARTIDOS PERDIDOS
-#local[4] + visita[6] = PUNTOS GANADOS
-#local[6] + visita[4] = PUNTOS PERDIDOS
+#local[3] + visita[1] = PUNTOS PERDIDOS
+#local[4] + visita[6] = PARTIDOS GANADOS
+#local[6] + visita[4] = PARTIDOS PERDIDOS
 
 rbind(total)
 
@@ -69,29 +69,63 @@ rbind(total)
 
 equipo = "Universidad Catolica" #ESCRIBIR BIEN EL NOMBRE DEL EQUIPO
 ## PREGUNTAR QUE HACER CON LA Ñ Y LOS ACENTOS
-
-Local <- Datos %>% 
-  filter(HT == equipo)  
+anio = "2009"
+partidos <- function(equipo,anio){
+  Local <- Datos %>% 
+    filter(HT == equipo)  
   
-Visita <- Datos %>% 
-  filter(AT == equipo)
- 
-local <- Local %>% 
-  filter(año == "2008") %>% 
-  puntos()
+  Visita <- Datos %>% 
+    filter(AT == equipo)
+  
+  local <- Local %>% 
+    filter(año == anio) %>% 
+    puntos()
+  
+  visita <- Visita %>% 
+    filter(año == anio) %>% 
+    puntos()
+  
+  rbind(local,visita)
 
-visita <- Visita %>% 
-  filter(año == "2008") %>% 
-  puntos()
+  
+  total <- c(local[1] + visita[3],local[2] + visita[2],local[3] + visita[1],local[4] + visita[6],local[5] + visita[5],local[6] + visita[4])
+  
+  return(rbind(total))
+}
 
-rbind(local,visita)
+partido <- partidos("Universidad Catolica","2020")
+g_09 <- partido[4]
+g_10 <- partido[4]
+g_11 <- partido[4]
+g_12 <- partido[4]
+g_13 <- partido[4]
+g_14 <- partido[4]
+g_15 <- partido[4]
+g_16 <- partido[4]
+g_17 <- partido[4]
+g_18 <- partido[4]
+g_19 <- partido[4]
+g_20 <- partido[4]
 
+ganados <- rbind(g_09,g_10,g_11,g_12,g_13,g_14,g_15,g_16,g_17,g_18,g_19,g_20)
 
-total <- c(local[1] + visita[3],local[2] + visita[2],local[3] + visita[1],local[4] + visita[6],local[5] + visita[5],local[6] + visita[4])
+t_09 <- partido[4]+partido[5]+partido[6]
+t_10 <- partido[4]+partido[5]+partido[6]
+t_11 <- partido[4]+partido[5]+partido[6]
+t_12 <- partido[4]+partido[5]+partido[6]
+t_13 <- partido[4]+partido[5]+partido[6]
+t_14 <- partido[4]+partido[5]+partido[6]
+t_15 <- partido[4]+partido[5]+partido[6]
+t_16 <- partido[4]+partido[5]+partido[6]
+t_17 <- partido[4]+partido[5]+partido[6]
+t_18 <- partido[4]+partido[5]+partido[6]
+t_19 <- partido[4]+partido[5]+partido[6]
+t_20 <- partido[4]+partido[5]+partido[6]
 
-rbind(total)
+#media de partidos por temporada
+total <- cbind(t_09,t_10,t_11,t_12,t_13,t_14,t_15,t_16,t_17,t_18,t_19,t_20)
+años <- c(2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020)
 
+plot(x=años,y=ganados)
 
-
-#Escribir en el README----
-## Los datos fueron obtenidos de la pagina kaggle: copio la url o [kaggle](url) asi si seleccion kaggle abre la url
+ggplot() + geom_point(aes(x=años,y=ganados)) + scale_y_continuous(limits = c(0,30))
